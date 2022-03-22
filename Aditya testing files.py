f4466690d90a1dcf28e5edf2e737af75d1089135
Legends-lxp <102133364+Legends-lxp@users.noreply.github.com>
2022-03-22 10:36:39 +0530

    Create Aditya testing files.py

---
+++ Aditya testing files.py
@@ -0,0 +1,17 @@
+int.("Aditya ").str(" legend-lxp")
+print(.)
+Aditya = "hackering".str(inpurt) 
+Aditya = await. hacking tools
+inpurt.edit
+inpurt.delete
+.hacking(Aditya pandit"""))
+print(" Aditya "))
+hackers team op
+baap hobb lowde ki olad))
+
+
+prawash = " hacking tools ") 
+await (.int(parawsh malviya. str(inpurt)))
+inpurt.edit
+Aditya
+#mode bye pandit
